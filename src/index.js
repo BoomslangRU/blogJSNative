a73f16515dc33568edc811e1953c75b7171dae24
BoomslangRU <eilinals@mail.ru>
2021-08-16 13:10:38 +0300

    Adding a loading indicator

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import { CreateComponent } from './components/create.component'
 import { FavoriteComponent } from './components/favorite.component'
 import { HeaderComponent } from './components/header.component'
+import { LoaderComponent } from './components/loader.component'
 import { NavigationComponent } from './components/navigation.component'
 import { PostsComponent } from './components/posts.component'
 
 new HeaderComponent('header')
 
 const navigation = new NavigationComponent('navigation')
+const loader = new LoaderComponent('loader')
 
 const create = new CreateComponent('create')
-const posts = new PostsComponent('posts')
+const posts = new PostsComponent('posts', { loader })
 const favorite = new FavoriteComponent('favorite')
 
 navigation.registerTabs([
